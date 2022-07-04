2389797710f42c7dfdabed0b67b0e8c41c48bac2
cfgop23 <cfgop23@gmail.com>
2022-07-04 17:28:59 +0900

    init

---
+++ class/pages/01-01-qqq/index.js
@@ -0,0 +1,7 @@
+export default function myPage() {
+    return (
+        <div>
+            <input type="text">Hello World</input>
+            </div>
+    )
+};
---
+++ class/pages/01-02-emotion/index.js
@@ -0,0 +1,14 @@
+import { Wrapper, EmailInput } from '../../styles/emotion'
+
+export default function EmotionPage() {
+
+    // here is javascript.
+
+    return (
+        <Wrapper>
+            Hello
+            <EmailInput type="text"></EmailInput>
+            <img src="/vercel.svg"></img>
+        </Wrapper>
+    )
+}
