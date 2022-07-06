9690d0f22fdafa1319e76f8b5883542caad0a7e6
cfgop23 <cfgop23@gmail.com>
2022-07-06 10:05:11 +0900

    day3 programmers

--- programmers/day3.js
+++ programmers/day3.js
@@ -32,3 +32,25 @@ function temperature(num) {
     console.log("조금 춥네요");
   }
 }
+
+//21.
+
+function days(month) {
+  if (month >= 1 && month <= 12) {
+    if (
+      month === 1 ||
+      month === 3 ||
+      month === 5 ||
+      month === 7 ||
+      month === 8 ||
+      month === 10 ||
+      month === 12
+    ) {
+      console.log("31");
+    } else if (month === 4 || month === 6 || month === 9 || month === 11) {
+      console.log("30");
+    } else {
+      console.log("28");
+    }
+  }
+}
