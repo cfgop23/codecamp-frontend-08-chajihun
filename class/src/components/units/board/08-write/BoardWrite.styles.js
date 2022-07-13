192aa8ed09e70a01ef221ed044ed028beacc1151
Chajihun <cfgop23@gmail.com>
2022-07-13 14:06:55 +0900

    day8 class

---
+++ class/src/components/units/board/08-write/BoardWrite.styles.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const RedInput = styled.input`
+  border-color: red;
+`;
+
+export const RedButton = styled.button`
+  border-color: red;
+  background-color: ${(props) => (props.qqq ? "yellow" : "default")};
+  // return props.qqq; ;
+`;
+
+// export default function qqq() {}
