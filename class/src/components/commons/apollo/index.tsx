8ae8cdb4b89afa0350e3638db1d7df4122d86bc1
Chajihun <cfgop23@gmail.com>
2022-08-04 13:40:06 +0900

    day23 class

--- class/src/components/commons/apollo/index.tsx
+++ class/src/components/commons/apollo/index.tsx
@@ -5,7 +5,7 @@ import {
   InMemoryCache,
 } from "@apollo/client";
 import { createUploadLink } from "apollo-upload-client";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import { accessTokenState } from "../../../commons/store";
 
@@ -17,7 +17,36 @@ interface IApolloSettingsProps {
 }
 
 export default function ApolloSettings(props: IApolloSettingsProps) {
-  const [accessToken] = useRecoilState(accessTokenState);
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  // // 1. 프리렌더링 예제 - process.browser 방법
+  // if (process.browser) {
+  //   console.log("지금은 브라우저다.");
+  //   const result = localStorage.getItem("accessToken");
+  //   console.log(result);
+  // } else {
+  //   console.log("지금은 프론트 서버다.(즉, yarn dev 프로그램 내부다.)");
+  //   const result = localStorage.getItem("accessToken");
+  //   console.log(result);
+  // }
+
+  // // 2. 프리렌더링 예제 - typeof window 방법
+  // if (typeof window !== "undefined") {
+  //   console.log("지금은 브라우저다.");
+  //   const result = localStorage.getItem("accessToken");
+  //   console.log(result);
+  // } else {
+  //   console.log("지금은 프론트 서버다.(즉, yarn dev 프로그램 내부다.)");
+  //   const result = localStorage.getItem("accessToken");
+  //   console.log(result);
+  // }
+
+  // 3. 프리렌더링 무시 - useEffect 방법
+  useEffect(() => {
+    console.log("지금은 브라우저다.");
+    const accessToken = localStorage.getItem("accessToken") || "";
+    setAccessToken(accessToken);
+  }, []);
 
   const uploadLink = createUploadLink({
     uri: "http://backend08.codebootcamp.co.kr/graphql",
